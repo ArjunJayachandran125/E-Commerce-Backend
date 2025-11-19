5b6ecfca325fea972f8a2b3f7d697b696e2b3921
Arjun Jayachandran <arjunjayan1999@gmail.com>
2025-11-19 18:48:04 +0530

    errorHandler

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import userRoutes from './routes/userRoute.js';
+import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 const app = express(); 
 
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,10 +1,10 @@
-const notFound = (req,res,next) => {
+export const notFound = (req,res,next) => {
     const error = new Error(`Not found - ${req.originalUrl}`);
     res.status(404);
     next(error);
 };
 
-const errorHandler = (err,req,res,next) => {
+export const errorHandler = (err,req,res,next) => {
     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     if (err.code === 11000) {
         const field = Object.keys(err.keyValue);
